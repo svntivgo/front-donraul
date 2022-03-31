fb5ddf616324e1843155fa99fd03ab824648ea36
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-31 16:08:45 -0500

    configuracion firebase

---
+++ src/firebase/credentials.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANUXMTui2wM_1LKertZMSshGGa0oPTvm8",
+  authDomain: "don-raul.firebaseapp.com",
+  projectId: "don-raul",
+  storageBucket: "don-raul.appspot.com",
+  messagingSenderId: "140131976956",
+  appId: "1:140131976956:web:6c4f8bc2f61a842bf53979"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp
