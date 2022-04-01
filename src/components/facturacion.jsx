1be8a48ce6a6a87dad4f869576f2f8cf00a44b4b
Santiago Barrera <santiago.barrerae@gmail.com>
2022-04-01 13:37:50 -0500

    input para ingresar cantidad factura nueva

--- src/components/facturacion.jsx
+++ src/components/facturacion.jsx
@@ -1,13 +1,81 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getAll } from "../actions/actions";
+import Table from "../features/Table";
 
-const Facturacion = () => {
+const Facturacion = ({inventario, getInventario}) => {
+
+  function verificarCantidad(input, cantidad) {
+    if (input.value > cantidad) {
+      alert("Excede la cantidad de productos")
+      input.value = ""
+    }
+
+    if (input.value < 0) {
+      alert("Debe ser un valor positivo")
+      input.value = ""
+    }
+  }
+
+
+
+  useEffect(() => {
+    getInventario();
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Nombre",
+        accessor: "nombre",
+      },
+      {
+        Header: "Precio",
+        accessor: "precio",
+      },
+      {
+        Header: "Cantidad",
+        accessor: "cantidad",
+      },
+      {
+        Header: "",
+        id: "total",
+        accessor: (data) => {
+          return (
+            <input
+              required
+              type="number"
+              placeholder="Cantidad"
+              min="0"
+              max={data.cantidad}
+              onChange={(e) => verificarCantidad(e.target, data.cantidad)}
+            ></input>
+          );
+        },
+      },
+    ],
+    []
+  );
+
   return (
     <>
       <h1>Facturacion</h1>
-      <h2>Tabla</h2>
+      <Table columns={columns} data={inventario} />
       <button>Facturar venta</button>
     </>
   );
-}
+};
 
-export default Facturacion
+const mapStateToProps = (state) => ({
+  inventario: state.container,
+});
+
+const url = "http://localhost:8080/api/productos";
+
+const mapDispatchToProps = (dispatch) => ({
+  getInventario() {
+    getAll(url, dispatch);
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Facturacion);
